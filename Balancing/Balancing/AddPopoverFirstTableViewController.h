197700f19203e9a9914c265487c12ca6550acfc4
greggS <greggscutum@gmail.com>
2012-12-16 22:45:11 +0100

    Popover with empty tableView is presented from + button i navBar

---
+++ Balancing/Balancing/AddPopoverFirstTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPopoverFirstTableViewController.h
+//  Balancing
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 16.12.2012.
+//  Copyright (c) 2012 AGDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPopoverFirstTableViewController : UITableViewController
+
+@end
