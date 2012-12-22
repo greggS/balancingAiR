0e372812bc99714f66f8a427eb3e56f7f1907e4b
greggS <greggscutum@gmail.com>
2012-12-22 12:17:05 +0100

    Added DetailView controllers for Rod and Support

---
+++ Balancing/Balancing/RodDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  RodDetailsViewController.m
+//  Balancing
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 22.12.2012.
+//  Copyright (c) 2012 AGDev. All rights reserved.
+//
+
+#import "RodDetailsViewController.h"
+
+@interface RodDetailsViewController ()
+
+@end
+
+@implementation RodDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Balancing/Balancing/SupportDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SupportDetailsViewController.h
+//  Balancing
+//
+//  Created by Grzegorz Krukiewicz-Gacek on 22.12.2012.
+//  Copyright (c) 2012 AGDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SupportDetailsViewController : UIViewController
+
+@end
